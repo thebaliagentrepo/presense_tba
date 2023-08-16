8d9f3b55f0782dfcd32262e6861e9c3a5ee67fb6
wilantarajaya <wilantarajaya.123@gmail.com>
2023-08-16 15:05:51 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <>
       <div className="font-poppins">
-        <Presense />
-        {/* <Login /> */}
+        {/* <Presense /> */}
+        <Login />
       </div>
     </>
   );
